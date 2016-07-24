5ace1c46675e899c2e548217aaf8b9c6dc2a93cf
Hector Hurtarte <hectorh30@gmail.com>
2016-07-24 12:34:37 -0600

    WIP: Implementar indicadores

--- app/components/app-graphics-municipio-main-metrics.js
+++ app/components/app-graphics-municipio-main-metrics.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  municipio: null,
+
+  porcentajeEjecucion: Ember.computed('municipio.gastoTotal', 'municipio.presupuestoActual', function() {
+    return this.get('municipio.gastoTotal')
+        / this.get('municipio.presupuestoActual')
+        * 100;
+  })
 });
