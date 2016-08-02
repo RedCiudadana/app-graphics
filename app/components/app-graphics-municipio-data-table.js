fe6cc2c72f7dcc076cf0a6ed85bbf3dd4ffe6816
Hector Hurtarte <hectorh30@gmail.com>
2016-08-02 13:52:13 -0600

    Implementar tabla con ordering

--- app/components/app-graphics-municipio-data-table.js
+++ app/components/app-graphics-municipio-data-table.js
@@ -1,4 +1,103 @@
 import Ember from 'ember';
+import Table from 'ember-light-table';
+import formatMoney from 'accounting/format-money';
+
+const computed = Ember.computed;
 
 export default Ember.Component.extend({
+  municipalidad: null,
+
+  table: null,
+
+  columns: computed(function() {
+    return [
+      {label: 'Rubro', valuePath: 'name'},
+      {label: 'Monto', valuePath: 'value'}
+    ];
+  }),
+
+  rows: null,
+
+  sortedRows: Ember.computed.sort('rows', '_sortingDefinition'),
+
+  _sortingDefinition: ['name'],
+
+  init() {
+    this._super(...arguments);
+
+    let municipalidad = this.get('municipalidad');
+
+    let data = Ember.A([
+      {
+        name: 'Servicios públicos generales',
+        value: formatMoney(parseInt(municipalidad['MArubro1']), 'Q')
+      },
+      {
+        name: 'Orden púlico y seguridad ciudadana',
+        value: formatMoney(parseInt(municipalidad['MArubro2']), 'Q')
+      },
+      {
+        name: 'Atención a desastres y gestión de riesgos',
+        value: formatMoney(parseInt(municipalidad['MArubro3']), 'Q')
+      },
+      {
+        name: 'Asuntos económicos',
+        value: formatMoney(parseInt(municipalidad['MArubro4']), 'Q')
+      },
+      {
+        name: 'Protección ambiental',
+        value: formatMoney(parseInt(municipalidad['MArubro5']), 'Q')
+      },
+      {
+        name: 'Urbanización y servicios comunitarios',
+        value: formatMoney(parseInt(municipalidad['MArubro6']), 'Q')
+      },
+      {
+        name: 'Salud',
+        value: formatMoney(parseInt(municipalidad['MArubro7']), 'Q')
+      },
+      {
+        name: 'Actividades deportivas y recreativas',
+        value: formatMoney(parseInt(municipalidad['MArubro8']), 'Q')
+      },
+      {
+        name: 'Educación',
+        value: formatMoney(parseInt(municipalidad['MArubro9']), 'Q')
+      },
+      {
+        name: 'Protección social',
+        value: formatMoney(parseInt(municipalidad['MArubro10']), 'Q')
+      },
+      {
+        name: 'Transacciones de la deuda pública',
+        value: formatMoney(parseInt(municipalidad['MArubro11']), 'Q')
+      }
+    ]);
+
+    this.set('rows', data);
+
+    this.set('table', new Table(
+      this.get('columns'),
+      this.get('sortedRows')
+    ));
+  },
+
+  actions: {
+    onColumnClick(column) {
+
+      if (!column.sorted) {
+        return;
+      }
+
+      let sortingDefinition = column.get('valuePath')
+        + ':'
+        + (column.ascending ? 'asc' : 'desc');
+
+      this.set('_sortingDefinition', [sortingDefinition]);
+
+      this.get('table').setRows([]);
+
+      this.get('table').addRows(this.get('sortedRows'));
+    }
+  }
 });
--- app/components/app-graphics-municipio-treemap-graphic.js
+++ app/components/app-graphics-municipio-treemap-graphic.js
@@ -13,6 +13,8 @@ let colors = [
 ];
 
 export default Ember.Component.extend({
+  municipalidad: null,
+
   chartOptions: {
     chart: {
       type: 'treemap'
@@ -31,64 +33,64 @@ export default Ember.Component.extend({
   },
 
   chartData: Ember.computed('munipcio', function() {
-    let municipio = this.get('municipio');
+    let municipalidad = this.get('municipalidad');
 
     // Info de los rubros
     return [{
       data: [
         {
           name: 'Servicios públicos generales',
-          value: parseInt(municipio['MArubro1']),
+          value: parseInt(municipalidad['MArubro1']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Orden púlico y seguridad ciudadana',
-          value: parseInt(municipio['MArubro2']),
+          value: parseInt(municipalidad['MArubro2']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Atención a desastres y gestión de riesgos',
-          value: parseInt(municipio['MArubro3']),
+          value: parseInt(municipalidad['MArubro3']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Asuntos económicos',
-          value: parseInt(municipio['MArubro4']),
+          value: parseInt(municipalidad['MArubro4']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Protección ambiental',
-          value: parseInt(municipio['MArubro5']),
+          value: parseInt(municipalidad['MArubro5']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Urbanización y servicios comunitarios',
-          value: parseInt(municipio['MArubro6']),
+          value: parseInt(municipalidad['MArubro6']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Salud',
-          value: parseInt(municipio['MArubro7']),
+          value: parseInt(municipalidad['MArubro7']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Actividades deportivas y recreativas',
-          value: parseInt(municipio['MArubro8']),
+          value: parseInt(municipalidad['MArubro8']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Educación',
-          value: parseInt(municipio['MArubro9']),
+          value: parseInt(municipalidad['MArubro9']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Protección social',
-          value: parseInt(municipio['MArubro10']),
+          value: parseInt(municipalidad['MArubro10']),
           color: colors[Math.floor(Math.random() * colors.length)]
         },
         {
           name: 'Transacciones de la deuda pública',
-          value: parseInt(municipio['MArubro11']),
+          value: parseInt(municipalidad['MArubro11']),
           color: colors[Math.floor(Math.random() * colors.length)]
         }
       ]
