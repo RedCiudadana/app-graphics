ebd4259e8af7fcbe5ba6c206bcac5e3493876d60
Hector Hurtarte <hectorh30@gmail.com>
2016-08-30 16:09:27 -0600

    WIP

---
+++ build.sh
@@ -0,0 +1,10 @@
+ember build\
+    -o /home/servir/Servir/Oficina/Freelances/RedCiudadana/Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets \
+&& rm /home/servir/Servir/Oficina/Freelances/RedCiudadana/Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/index.html \
+&& rm /home/servir/Servir/Oficina/Freelances/RedCiudadana/Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/crossdomain.xml \
+&& rm /home/servir/Servir/Oficina/Freelances/RedCiudadana/Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/robots.txt \
+&& rm /home/servir/Servir/Oficina/Freelances/RedCiudadana/Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/testem.js \
+&& rm -rf /home/servir/Servir/Oficina/Freelances/RedCiudadana/Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/tests \
+&& mv /home/servir/Servir/Oficina/Freelances/RedCiudadana/Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/assets/* \
+    /home/servir/Servir/Oficina/Freelances/RedCiudadana/Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets \
+&& rm -rf /home/servir/Servir/Oficina/Freelances/RedCiudadana/Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/assets
