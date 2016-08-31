dbd07ac32487484209b905c35e3e8f99567cd4a6
Hector Hurtarte <hectorh30@gmail.com>
2016-08-31 10:35:12 -0600

    Clean up stuff

--- build-prod.sh
+++ build-prod.sh
@@ -1,8 +1,8 @@
 ember build\
+    -prod \
     -o ./../../../Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets \
 && rm ./../../../Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/index.html \
 && rm ./../../../Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/crossdomain.xml \
-&& rm ./../../../Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/testem.js \
 && rm ./../../../Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/robots.txt \
 && rm -rf ./../../../Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/tests \
 && mv ./../../../Dondevanmisimpuestos/soft/dondevanmisimpuestos-red/assets/assets/* \
