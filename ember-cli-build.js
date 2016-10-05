ba5ca5225b61286685bd83e3fa9b78cb276b9d63
Hector Hurtarte <hectorh30@gmail.com>
2016-10-05 16:14:26 -0600

    Fix image

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,7 +7,7 @@ module.exports = function(defaults) {
     storeConfigInMeta: false,
     'ember-bootstrap': {
       // TODO: This shall be changed to false when going prod
-      importBootstrapCSS: true
+      importBootstrapCSS: false
     },
     'ember-power-select': {
       theme: 'bootstrap'
