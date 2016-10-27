265b5efe49507065038bdd0eb73b5780b8f0913e
Hector Hurtarte <hectorh30@gmail.com>
2016-10-27 08:45:49 -0600

    Implementar versión inicial de 'compara'

--- app/controllers/compara.js
+++ app/controllers/compara.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  municipalidades: null,
+  municipalidadLeft: null,
+  municipalidadRight: null,
+
+  actions: {
+    selectMunicipalidadLeft(municipalidad) {
+      this.set('municipalidadLeft', municipalidad);
+    },
+
+    selectMunicipalidadRight(municipalidad) {
+      this.set('municipalidadRight', municipalidad);
+    }
+  }
 });
--- app/routes/compara.js
+++ app/routes/compara.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  setupController(controller, model) {
+    let municipalidades = this.modelFor('application').municipalidades;
+
+    controller.set('municipalidades', municipalidades);
+
+    console.log(municipalidades);
+
+    if (municipalidades.length >= 2) {
+      controller.set('municipalidadLeft', municipalidades.objectAt(0));
+      controller.set('municipalidadRight', municipalidades.objectAt(1));
+    }
+  }
 });
